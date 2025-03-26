a39551c6e94fa87b26927eedd1ec4a18d2a0d4a5
SGL314 <a2023951482@teiacoltec.org>
2025-03-26 15:39:51 -0300

    divisa de fogo varão de guerra

--- aula3/MainWindow.axaml.cs
+++ aula3/MainWindow.axaml.cs
@@ -1,35 +1,146 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System;
 
 namespace MeuProjetoAvalonia
 {
 	public partial class MainWindow : Window
 	{
         int qt = 0;
+		string typeConversion = "nothing";
     	public MainWindow()
     	{
         	// Inicializa a interface a partir do XAML
         	InitializeComponent();
+			allInvisible();
     	}
 
-    	// Evento chamado quando o botão é clicado
-    	private void BtnClique_Click(object sender, RoutedEventArgs e)
+    	private void converter(object sender, RoutedEventArgs e){
+			float entrada = float.Parse(input.Text.ToString());
+			float resultado = 0;
+        	switch (typeConversion){
+				case "°C > °F":
+					resultado = entrada*1.8f+32;
+					break;
+				case "°C > °K":
+					resultado = entrada + 273.15f;
+					break;
+				case "°F > °C":
+					resultado = (entrada-32)/1.8f;
+					break;
+				case "°F > °K":
+					resultado = entrada - 273.15f;
+					break;
+				case "m > ft":
+					resultado = entrada * 3.28084f;
+					break;
+				case "ft > m":
+					resultado = entrada / 3.28084f;
+					break;
+				case "km > mi":
+					resultado = entrada / 1.60934f;
+					break;
+				case "mi > km":
+					resultado = entrada * 1.60934f;
+					break;
+				case "kg > lb":
+					resultado = entrada * 2.20462f;
+					break;
+				case "lb > kg":
+					resultado = entrada / 2.20462f;
+					break;
+				case "g > oz":
+					resultado = entrada / 28.3495f;
+					break;
+				case "oz > g":
+					resultado = entrada * 28.3495f;
+					break;
+				case "L > gal":
+					resultado = entrada / 3.78541f;
+					break;
+				case "gal > L":
+					resultado = entrada * 3.78541f;
+					break;
+				case "ml > fl oz":
+					resultado = entrada / 29.5735f;
+					break;
+				case "fl oz > ml":
+					resultado = entrada * 29.5735f;
+					break;
+				default:
+					break;
+			}
+			output.Text = entrada.ToString()+typeConversion.Split(" > ")[0]+" > "+resultado.ToString()+typeConversion.Split(">")[1];
+    	}
+
+		private void allInvisible()
     	{
-        	// Recupera o valor digitado no TextBox pelo nome definido no XAML (txtInput)
-        	var nome = this.FindControl<TextBox>("txtInput").Text;
-            qt ++;
-
-        	// Exibe uma mensagem alterando o texto do próprio TextBox como exemplo
-        	if (!string.IsNullOrEmpty(nome))
-        	{
-                if (qt == 1)
-            	this.FindControl<TextBox>("txtInput").Text = $"Olá, {nome}!";
-        	}
-        	else
-        	{
-            	// Caso o usuário não tenha digitado nada, exibe uma mensagem padrão
-            	this.FindControl<TextBox>("txtInput").Text = "Olá, visitante!";
-        	}
+			
+			Temperatura.IsVisible = false;
+			Massa.IsVisible = false;
+			Comprimento.IsVisible = false;
+			Volume.IsVisible = false;
+			
     	}
+
+		private void drop_changed(object sender, SelectionChangedEventArgs e)
+		{
+			var comboBox = sender as ComboBox;
+			if (comboBox.SelectedItem is ComboBoxItem item)
+			{
+				switch (item.Content){
+					case "Temperatura":
+						allInvisible();
+						Temperatura.IsVisible = true;
+						break;
+					case "Massa":
+						allInvisible();
+						Massa.IsVisible = true;
+						break;
+					case "Volume":
+						allInvisible();
+						Volume.IsVisible = true;
+						break;
+					case "Comprimento":
+						allInvisible();
+						Comprimento.IsVisible = true;
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	
+		private void mass_changed(object sender, SelectionChangedEventArgs e){
+			var vari = sender as ListBox;
+            if (vari.SelectedItem is ListBoxItem item){
+                typeConversion = item.Content.ToString();
+			}
+			output.Text = (string) (typeConversion);
+        }
+
+		private void comp_changed(object sender, SelectionChangedEventArgs e){
+            var vari = sender as ListBox;
+            if (vari.SelectedItem is ListBoxItem item){
+                typeConversion = item.Content.ToString();
+			}
+			output.Text = (string) (typeConversion);
+        }
+
+		private void volu_changed(object sender, SelectionChangedEventArgs e){
+            var vari = sender as ListBox;
+            if (vari.SelectedItem is ListBoxItem item){
+                typeConversion = item.Content.ToString();
+			}
+			output.Text = (string) (typeConversion);
+        }
+
+		private void temp_changed(object sender, SelectionChangedEventArgs e){
+            var vari = sender as ListBox;
+            if (vari.SelectedItem is ListBoxItem item){
+                typeConversion = item.Content.ToString();
+			}
+			output.Text = (string) (typeConversion);
+        }
 	}
 }
