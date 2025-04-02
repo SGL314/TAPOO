4fa6a72f6c644ff9243aa2054aa20f56a9a3e255
SGL314 <a2023951482@teiacoltec.org>
2025-04-02 15:22:59 -0300

    wow

---
+++ aula4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+Random rd = new Random();
+
+Thread chef = new Thread(() => {
+	
+});
+
+Thread garcom = new Thread(() => {
+	while (true){
+        int time = (int) (rd.NextDouble()*10+1);
+        Thread.Sleep(time);
+        // Console.WriteLine("" + time);
+    }
+});
+
+garcom.Start();
+
+Console.WriteLine("Thread finalizada. Continuando a execução...");
+
+
+
