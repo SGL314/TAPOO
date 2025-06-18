5e373c29c5a36294e0e1fdf194b03cbd2c519e60
SGL314 <samugonlob12@gmail.com>
2025-06-18 17:00:11 -0300

    aula4

--- 2trim/aula4/Program.cs
+++ 2trim/aula4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+var sw = Stopwatch.StartNew();
+var initialMemory = GC.GetTotalMemory(true);
+
+ImageProcessor.ProcessImages(500);
+
+var finalMemory = GC.GetTotalMemory(true);
+sw.Stop();
+
+Console.WriteLine($"Memória inicial: {initialMemory / 1024.0:F2} KB");
+Console.WriteLine($"Memória final: {finalMemory / 1024.0:F2} KB");
+Console.WriteLine($"Diferença de memória: {(finalMemory - initialMemory) / 1024.0:F2} KB");
+Console.WriteLine($"Coleções GC Gen0: {GC.CollectionCount(0)}");
+Console.WriteLine($"Coleções GC Gen1: {GC.CollectionCount(1)}");
+Console.WriteLine($"Coleções GC Gen2: {GC.CollectionCount(2)}");
